a097b5634a9e453a4d0367faa6858a2eb5397687
arielmatius <arielmatius21@gmail.com>
2022-11-25 06:20:53 +0700

    initial update

---
+++ Modul7/app/src/main/java/com/example/modul7/helper/MappingHelper.kt
@@ -0,0 +1,24 @@
+package com.example.modul7.helper
+
+import android.database.Cursor
+import com.example.modul7.Homework
+import com.example.modul7.db.DatabaseContract
+
+object MappingHelper {
+
+    fun mapCursorToArrayList(homeworkCursor: Cursor?): ArrayList<Homework> {
+        val homeworkList = ArrayList<Homework>()
+
+        homeworkCursor?.apply {
+            while (moveToNext()) {
+                val id = getInt(getColumnIndexOrThrow(DatabaseContract.HomeworkColumns._ID))
+                val title = getString(getColumnIndexOrThrow(DatabaseContract.HomeworkColumns.TITLE))
+                val description =
+                    getString(getColumnIndexOrThrow(DatabaseContract.HomeworkColumns.DESCRIPTION))
+                val date = getString(getColumnIndexOrThrow(DatabaseContract.HomeworkColumns.DATE))
+                homeworkList.add(Homework(id, title, description, date))
+            }
+        }
+        return homeworkList
+    }
+}
